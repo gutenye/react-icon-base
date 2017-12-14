faad5f01b00107a43f1a15959ae6b637674a1fed
Guten Ye <ywzhaifei@gmail.com>
2017-12-14 19:54:04 +0800

    Guten version

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import { default as React, PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = {} }) => {
-  const computedSize = size || reactIconBase.size || '1em'
+const IconBase = ({ children, color, size, style, large, small, className, ...props }, { reactIconBase = {} }) => {
+  var computedSize = size || reactIconBase.size || large ? '2em' : small ? '1em' : '1.3em'
   return (
-    <svg
+    <Svg
+      className={`icon ${className || ''}`}
       children={children}
       fill='currentColor'
       preserveAspectRatio='xMidYMid meet'
@@ -20,6 +23,14 @@ const IconBase = ({ children, color, size, style, ...props }, { reactIconBase = 
     />
   )
 }
+
+const Svg = styled.svg`
+  cursor: ${p => p.button ? 'pointer' : 'inherit'};
+  color: ${p => p.gray ? '#c6cbd1' : 'inherit'};
+  &:hover {
+    color: ${p => p.gray ? '#959da5' : 'inherit'};
+  }
+`
 
 IconBase.propTypes = {
   color: PropTypes.string,
